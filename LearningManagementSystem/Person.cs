6aaad4e9a27c8ce7890da31856713e197679fdda
babich kyrylo <babich912@gmail.com>
2023-09-24 19:24:36 +0300

    Add project files.

---
+++ LearningManagementSystem/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem
+{
+    abstract class Person
+    {
+        public required int Id { get; init; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public readonly string Course;
+
+        public Person(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
