9baf1e252769d08d18a76122585a17ebf5229e3b
ognyp <Oksankagnyp@gmail.com>
2023-09-30 20:13:56 +0300

    Created SetEstimate method

--- LearningManagementSystem/Teacher.cs
+++ LearningManagementSystem/Teacher.cs
@@ -1,6 +1,45 @@
 ﻿namespace LearningManagementSystem
 {
-    public class Teacher
+    internal class Teacher : Person
     {
+        public const string SchoolName = "EduCenter";
+
+        public int workingHours { get; set; }
+
+        public Teacher(int id, string firstName, string lastName) : base(id, firstName, lastName)
+        {
+        }
+
+        public string SetEstimation(double percentOfCorrectAnswers)
+        {
+            string estimate = "";
+
+            switch (percentOfCorrectAnswers)
+            {
+                case >= 90:
+                    estimate = "A - perfect";
+                    break;
+                case >= 82:
+                    estimate = "B - very well";
+                    break;
+                case >= 75:
+                    estimate = "C - good";
+                    break;
+                case >= 67:
+                    estimate = "D - satisfactorily";
+                    break;
+                case >= 60:
+                    estimate = "E - enough satisfactorily";
+                    break;
+                case >= 35:
+                    estimate = "FX - not enough, with possibility to retake";
+                    break;
+                default:
+                    estimate = "F - retake the course";
+                    break;
+            }
+
+            return estimate;
+        }
     }
 }
