6c503a0d27f2758a60067d6c3941b5a1d6c8b50b
babich kyrylo <babich912@gmail.com>
2023-10-03 11:12:23 +0300

    Add extension class and record

---
+++ LearningManagementSystem/AppExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem
+{
+    internal static class AppExtensions
+    {
+        public static bool HasStudent(this Course course, int studentId)
+        {
+            return course is not null && course.StudentsIds.Contains(studentId);
+        }
+    }
+}
